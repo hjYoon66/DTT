693ab3497ad0edd041bce36b8d336d2c6ac167e8
Yoon_98 <poiu601@naver.com>
2023-04-30 22:52:23 +0900

    버튼 클릭시 테이블 상태 변경 구현 완료

--- src/main/java/carpe/dtt/controller/TableController.java
+++ src/main/java/carpe/dtt/controller/TableController.java
@@ -1,23 +1,32 @@
 package carpe.dtt.controller;
 
-import carpe.dtt.repository.TableRepository;
 import carpe.dtt.service.TableService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/table")
 public class TableController {
-    @Autowired
-    private TableService tableService;
+    private final TableService tableService;
+
+    public TableController(TableService tableService) {
+        this.tableService = tableService;
+    }
 
     @GetMapping("/{id}/status")
-    public ResponseEntity<Integer> getStatusById(@PathVariable("id") long id) {
+    public ResponseEntity<Integer> getStatusById(@PathVariable Long id) {
         Integer status = tableService.getStatusById(id);
         return ResponseEntity.ok(status);
+    }
+    @PostMapping("/changeY/{id}")
+    public Integer changeDataUseY(@PathVariable Long id) {
+        tableService.changeDataUseY(id);
+        return tableService.getStatusById(id);
+    }
+    @PostMapping("/changeN/{id}")
+    public Integer changeDatUseN(@PathVariable Long id) {
+        tableService.changeDataUseN(id);
+        return tableService.getStatusById(id);
     }
 }
--- src/main/java/carpe/dtt/service/TableService.java
+++ src/main/java/carpe/dtt/service/TableService.java
@@ -2,17 +2,22 @@ package carpe.dtt.service;
 
 import carpe.dtt.entity.Table;
 import carpe.dtt.repository.TableRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Optional;
 
 @Service
 public class TableService {
-    @Autowired
-    private TableRepository tableRepository;
+    private final TableRepository tableRepository;
 
-    public Integer getStatusById(Long id){
+    public TableService(TableRepository tableRepository) {
+        this.tableRepository = tableRepository;
+    }
+    @Transactional
+    public Integer getStatusById(Long id) {
         Optional<Table> optionalTable = tableRepository.findById(id);
         if (optionalTable.isPresent()) {
             Table table = optionalTable.get();
@@ -22,5 +27,20 @@ public class TableService {
         }
     }
 
-
+    @Transactional
+    public void changeDataUseY(Long id) {
+        Table table= tableRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid data Id:" + id));
+        // 데이터 수정 작업 수행
+        table.setStatus(1);
+        tableRepository.save(table);
+    }
+    @Transactional
+    public void changeDataUseN(Long id) {
+        Table table= tableRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid data Id:" + id));
+        // 데이터 수정 작업 수행
+        table.setStatus(0);
+        tableRepository.save(table);
+    }
 }
